72c28dc1638f171f7acd4e2db51393ae92e2b488
ZarelLast <triwibowoilham2@gmail.com>
2025-03-26 15:38:03 +0700

    feat(setup): initial commit

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import { fileURLToPath, URL } from 'node:url'
+import Components from 'unplugin-vue-components/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Components({
+      directoryAsNamespace: true
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    chuckSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
+})
