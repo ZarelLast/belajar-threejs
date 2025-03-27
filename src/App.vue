d4ae80ce64f0ff1ded1c6b9879d3443d651654f3
ZarelLast <triwibowoilham2@gmail.com>
2025-03-27 14:42:34 +0700

    fix(setup): fix urutan lint-staged

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
-let data = "cihuy"
+let data = "cihuy";
 </script>
 
 <template>
--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,36 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import { fileURLToPath, URL } from 'node:url'
-import Components from 'unplugin-vue-components/vite'
+import { fileURLToPath, URL } from "node:url";
+import Components from "unplugin-vue-components/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Components({
-      directoryAsNamespace: true
-    })
+      directoryAsNamespace: true,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   build: {
     chuckSizeWarningLimit: 1600,
     rollupOptions: {
       output: {
         manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
           }
-        }
-      }
-    }
-  }
-})
+        },
+      },
+    },
+  },
+});
