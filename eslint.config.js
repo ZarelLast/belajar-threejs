09effe062e9f7bdbeead011a388337bd1eb5c946
ZarelLast <triwibowoilham2@gmail.com>
2025-03-27 14:35:57 +0700

    feat(setup): add eslint rules & add prettier to package.json lint-staged

--- eslint.config.js
+++ eslint.config.js
@@ -4,16 +4,16 @@ import js from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
 
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs,vue}"], 
-    languageOptions: { globals: globals.browser }, 
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    languageOptions: { globals: globals.browser },
     plugins: { js, vue: pluginVue },
     extends: ["js/recommended", pluginVue.configs["flat/essential"]],
     rules: {
       "vue/multi-word-component-names": "off", // Contoh: Mematikan aturan nama multi-kata
       "vue/html-indent": ["error", 2], // Indentasi HTML 2 spasi
-      "vue/no-unused-vars": "warn", // Peringatan jika ada variabel Vue yang tidak terpakai
+      "vue/no-unused-vars": ["error"], // Peringatan jika ada variabel Vue yang tidak terpakai
       "vue/no-console": "warn", // Peringatan jika ada perintah console.log
-    }
-  }
+    },
+  },
 ]);
